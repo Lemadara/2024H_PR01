eb8a3a585fb22b29c7e3df2215b3b7366cd56562
Lemadara <148708287+Lemadara@users.noreply.github.com>
2024-02-02 11:34:03 -0500

    game complete, now need to translate all english to french or french to english, ask for the requirements in class to the teacher

--- mainOne.py
+++ mainOne.py
@@ -2,169 +2,354 @@ import os
 from util import lire_historique_utilisateur, enregistrer_partie, lire_dictionnaires_mots
 import random
 import time
-#import keyboard
-
-
-
+import string
+import keyboard
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def menuNchoise():
-	clear_console()
-	print("****MENU****\n"," 1 Commencer une partie\n"," 2 Afficher l'historique \n"," 3 Quitter\n","\n")
+	print("Menu principal:\n","1. Commencer une partie\n","2. Afficher l'historique \n","3. Quitter\n")
 
 def diffMenu():
-	diffValue=int(input("1 Facile\n2 Intermédiaire\n3 Difficile\n"))
-	clear_console()
+	diffValue=input("Choisir une difficulté:\n1. Facile\n2. Intermédiaire\n3. Difficile\n")
 	return diffValue
 
 def gameword(diff):
 	exiter=0
+	diC=lire_dictionnaires_mots()
 	while True:
 
-		if diff==1:
-			diC=lire_dictionnaires_mots()
+		if diff=="1":
 			motList=diC["facile"]
 			wordChoosen=random.choice(motList)
-	    
-			print(wordChoosen)
 
-			d=input('waiter')
 			exiter=1
 
-		elif diff==2:
-			diC=lire_dictionnaires_mots()
+		elif diff=="2":
 			motList=diC["intermediaire"]
 			wordChoosen=random.choice(motList)
-	    
-			print(wordChoosen)
 
-			d=input('waiter')
 			exiter=1
 
-		elif diff==3:
-			diC=lire_dictionnaires_mots()
+		elif diff=="3":
 			motList=diC["difficile"]
 			wordChoosen=random.choice(motList)
 	    
-			print(wordChoosen)
-
-			d=input('waiter')
 			exiter=1
 
 		else:
-			print("Enter again or type 4 for exit")
 			while True:
+				clear_console()
+				print("Choisir une difficulté:\n1. Facile\n2. Intermédiaire\n3. Difficile\n")
+				print("Erreur, entrée invalide, entrer un nombre de 1 à 3 de la selection :")
+
 				diff=input(" : ")
-				if diff==4:
-					exiter=1
+				if diff in ['1','2','3']:
 					break
+				else:
+					...
 
 		if exiter ==1:
-			return wordChoosen
+			break
+	return wordChoosen
+
+def printOnwTries(wtries):
 
 
+	if wtries==0:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("      |")
+		print("      |")
+		print("      |")
+		print("      | ")
+		print("=========")
+	elif wtries==1:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("  O   |")
+		print("      |")
+		print("      |")
+		print("      |  ")
+		print("=========")
+	elif wtries ==2:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("  O   |")
+		print("  |   |")
+		print("      |")
+		print("      |  ")
+		print("=========")
+	elif wtries ==3:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("  O   |")
+		print(" /|   |")
+		print("      |")
+		print("      |  ")
+		print("=========")
+	elif wtries ==4:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("  O   |")
+		print(" /|\\  |")
+		print("      |")
+		print("      |  ")
+		print("=========")
+	elif wtries ==5:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("  O   |")
+		print(" /|\\  |")
+		print(" /    |")
+		print("      |  ")
+		print("=========")
+	elif wtries ==6:
+
+		print("")
+		print("  +---+")
+		print("  |   |")
+		print("  O   |")
+		print(" /|\\  |")
+		print(" / \\  |")
+		print("      |  ")
+		print("=========")
+
+def gameHeaderInfo(elWord,letterFound,wtries,letterRecord):
+	clear_console()
+	print("admin info : word = ",elWord) # to retract # admin stuff
+
+	#word visual - found letters and progression on finding the word
+	print("word : ",end="")
+
+	for x in elWord:
+		if x in letterFound:
+			print(x,end=" ")
+		else:
+			print("_",end=" ")
+	print("\nletter Found: ",end='')
+	for c in letterFound:
+		print(c,end=" ")
+	print('')
+	print('letter Mistakes: ',end='')
+	for z in letterRecord:
+		if z not in elWord:
+			print(z,end=' ')
+
+
+	# Le bonhomme pendu drawings progressing on numbers of wtries/number of wrong letters entered
+	printOnwTries(wtries)
+	pass
 
 def game(elWord):
-	tries=0
-	listE=[]
-	###
-	start_time = time.time()
-
-	multi=len(elWord)
-	listW=elWord.split()
-
-	###
-
-	letter=input("give a letter")
-	if letter not in listE and letter in listW:
-		listE.append(letter)
-		tries+=1
-
-	if letter in listW:
-		position=listW.index(letter)
-
-	else:
-		print("error try again with a real letter")
-
+	# initial datas
+	positionned=[]
+	for x in elWord:
+		positionned.append(x)
+	print(positionned)
+	letterFound=[]
+	letterRecord=[]	
+	wtries=0
+	letterAlreadyEntered=False
+	gtries=0
+	
+	#where the while begins
 	while True:
-		print(f"mots:"+"_"*multi)
-		print(f"lettre trouvé:"+listW[position])
-		print(f"")
-		print(f"")
-		print(f"")
-		print(f"")
-		print(f"")
+		gameHeaderInfo(elWord,letterFound,wtries,letterRecord)
+		"""clear_console()
+								print("admin info : word = ",elWord) # to retract # admin stuff
+						
+								#word visual - found letters and progression on finding the word
+								print("word : ",end="")
+						
+								for x in elWord:
+									if x in letterFound:
+										print(x,end=" ")
+									else:
+										print("_",end=" ")
+						
+								# Le bonhomme pendu drawings progressing on numbers of wtries/number of wrong letters entered
+								printOnwTries(wtries)"""
+
+		if letterAlreadyEntered:
+			if letter in letterRecord:
+				print("letter already entered, try again : ",end='')
+		#		print('\n',len(letterFound),len(positionned))
+		elif not letterAlreadyEntered:
+			print("try to guess a letter of the word : ",end='')
+		#	print('\n',len(letterFound),len(positionned))
 
 
-	end_time = time.time()
-	time = end_time-start_time
-	print(time)
-	return 0
+		letter=input("")
 
+		if not(letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] or letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']):
+			while True:
+				gameHeaderInfo(elWord,letterFound,wtries,letterRecord)
 
-clear_console()
+				print("Error not a letter; \nTry again with a letter : ",end='')
+				letter=input()
+				if letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] or letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
 
+					break
+		
+		letter=letter.lower()
 
-### code start ########################################################################################################################################
+		# detect when a new letter is entered
+		if letter not in letterRecord: # new letter
+			letterRecord.append(letter)
+			if letter in positionned: # in the word
+				for x in positionned:
+					if x ==letter:
+						gtries+=1
+				letterFound.append(letter)
+			elif letter not in positionned: # not in the word
+				wtries+=1
 
-# user name
-print("give user name\n")
-userName = input(" : ")
+			letterAlreadyEntered=False
+
+		elif letter in letterRecord:
+
+			letterAlreadyEntered=True
+
+		# we win or loose # when game ends
+		if gtries==len(positionned):
+			gameHeaderInfo(elWord,letterFound,wtries,letterRecord)
+			return([True,wtries])
+		elif wtries==7:
+			return([False,wtries])
+
+def welcome():
+	while True:
+		
+		print("Entrer votre nom d'utilisateur:",end=' ')
+		userName = input()
+		clear_console()
+		#splitedUName= list(userName)# not needed
+		l=0
+
+		if len(userName)>=3:
+			print('')
+
+		elif len(userName)<3:
+			clear_console()
+			print('error: less than 3 letters\n')
+			l=1
+
+		for x in userName:
+			if x in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] or x in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+				...
+			else:
+				print("char:",x)
+				print(f"error : uncorrect {x} not a letter")
+				l=1
+
+		if l==0:
+			return userName
+		
+		print("\nplease try again with only letters and at least 3 letters\n")
+
+### code start program ########################################################################################################################################
+
+# welcome and userName returned
+userName=welcome()
+print("your userName is : "+userName)	
+input("..enter..")
+
 # menu first apparition
-
-menuNchoise()
-choiseM=int(input())
 clear_console()
+menuNchoise()
+choiseM=input()
 
 k=1 # value to control when we go to menu level 1
 
 # main events navigation
-
 while True:
 	if k==0:
-
-		menuNchoise()
-		choiseM=int(input("Make a choice : "))
 		clear_console()
+		menuNchoise()
+		choiseM=input("Make a choice : ")
 
-	if choiseM==1:
+	if choiseM=='1':
+		clear_console()
 		choiDiff=diffMenu()
 
+		#difficulties menu and choise - return choosen difficulty
+
 		elWord=gameword(choiDiff)
+		#generate El Word based on the difficulty choosen
 
-		print(elWord)
+		start_time = time.time()
 
-		input("waiter")
+		# game starts
+		clear_console()
+		result=game(elWord) # game result stored in result
 
-		game(elWord)
+		end_time = time.time()
+
+		fTime = end_time-start_time
+		iTime=int(fTime)
+
+		if result[0]:
+			print(f"Victoire : Félicitations '{userName}'! Vous avez deviné le mot '{elWord}' en {iTime} secondes et {result[1]} tentatives échouées.")
+		elif not result[0]:
+			print(f"Défaite : Dommage ! Le mot était {elWord}")
+
+		print("Appuyez sur Entrée pour continuer vers le menu principal")
+		#keyboard.wait('enter')
+		#time.sleep(1)
+
+		while True:
+			p=input()
+			if p =='':
+				break
+
+		enregistrer_partie(userName,elWord,result[0],int(fTime))
 
 		k=0
 		
-	elif choiseM==2:
+	elif choiseM=='2':
 
 		# historic looking later
+		clear_console()
+		liste_historic=lire_historique_utilisateur(userName)
+		#print(liste_historic)
+		print("Historique des parties de '{userName}':\n")
+		for x in liste_historic:
+			resultHistoric= 'victoire' if x['resultat'] else 'defaite'
+			print(f'date et heure : {x['timestamp']}; mot : {x['mot']}; resultat : {resultHistoric}; temps : {x['duree']} secondes\n')
 
+		print("press any keys to go back")
+		input('...press a key...')
 
 		k=0
 		
-	elif choiseM==3:
-
-		##FINISHED##
+	elif choiseM=='3':
+		clear_console()
+		##quit program##
+		print(f"merci d'avoir entrer le programme '{userName}', à une prochaine fois!")
 		
 		break
 
 	else:
-		while choiseM not in [1,2,3]:
-			
-			menuNchoise()
-			print("error wrong number or character")
-			choiseM=int(input(" give it again : "))
+		while choiseM not in ['1','2','3']:
 			clear_console()
+			menuNchoise()
+			print("error wrong number or a character or special character is entered")
+			choiseM=input("please try again : ")
 			k=1
 
 
-	clear_console()	
-
-print("exited")
+# program ended
--- test.py
+++ test.py
@@ -81,79 +81,164 @@ def printOnwTries(wtries):
 		print("=========")
 
 def game(elWord):
-	###	Game starts
 	# initial datas
-	position={}	
 	positionned=[]
 	for x in elWord:
-		if x not in positionned:
-			position[x]=[]
-			positionned.append(x)
+		positionned.append(x)
+	print(positionned)
 	letterFound=[]
-	letterRecord=[]
-
-	tries=0
+	letterRecord=[]	
 	wtries=0
-
-
+	letterAlreadyEntered=False
+	gtries=0
+	
 	#where the while begins
 	while True:
-
 		print("admin info : word = ",elWord) # to retract # admin stuff
 
+		#word visual - found letters and progression on finding the word
 		print("word : ",end="")
-		### where to implant it
+
 		for x in elWord:
 			if x in letterFound:
 				print(x,end=" ")
 			else:
 				print("_",end=" ")
 
+		# Le bonhomme pendu drawings progressing on numbers of wtries/number of wrong letters entered
 		printOnwTries(wtries)
 
-		print("try to guess a letter of the word :")
+		if letterAlreadyEntered:
+			if letter in letterRecord:
+				print("letter already entered, try again : ",end='')
+				print('\n'+len(letterFound)+len(positionned))
+		elif not letterAlreadyEntered:
+			print("try to guess a letter of the word : ",end='')
+			print('\n'+len(letterFound)+len(positionned))
 
-		letter=input(" : ")
-		if letter not in positionned:
+
+		letter=input("")
+
+		if not(letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] or letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']):
+			while True:
+				print("Error not a letter; \nTry again with a letter : ",end='')
+				letter=input()
+				if letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] or letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+					break
+
+
+		if letter not in positionned and letter not in letterRecord: # hmm wrong try if letter not in the word and if letter not in the record of entered letter
 			wtries+=1
-
-		tries+=1
-
-
-		for i in range(len(elWord)):
-			if letter == elWord[i]:
-				position[letter].append(i)
-
-		print(position)# to retract # admin stuff
+		if letter in positionned and letter not in letterRecord:
+			gtries+=1
 			
-			# detect when a new letter is entered
-		if letter in elWord and letter not in letterRecord:	
-
-			letterFound.append(letter)
+		# detect when a new letter is entered
+		if letter not in letterRecord:
 			letterRecord.append(letter)
+			if letter in positionned:
+				letterFound.append(letter)
+			letterAlreadyEntered=False
 
-		elif letter not in letterRecord and letter not in elWord:	# record entered characters
+		elif letter in letterRecord:
+			letterAlreadyEntered=True
 
-			letterRecord.append(letter)
-
-		if len(letterFound)==len(positionned):	# when we win or loose # when game ends
-			return("won")
-		elif wtries==6:
-			return("lost")
+		# we win or loose # when game ends
+		if gtries==len(positionned):
+			return(["won",wtries])
+		elif wtries==7:
+			return(["lost",wtries])
 #	return 0
 
+def welcome():
+	while True:
+		print("Entrer votre nom d'utilisateur:",end=' ')
+		userName = input()
+		#splitedUName= list(userName)# not needed
+		l=0
 
+		if len(userName)>3:
+			print('')
+		elif len(userName)<3:
 
+			print('error: less than 3 letters\n')
+			l=1
+
+		for x in userName:
+			if x in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] or x in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']:
+				...
+			else:
+				print("char:",x)
+				print(f"error : uncorrect {x} not a letter")
+				l=1
+
+		if l==0:
+			return userName
+		
+		print("\nplease try again with only letters and at least 3 letters\n")
+def gameword(diff):
+	exiter=0
+	diC=lire_dictionnaires_mots()
+	while True:
+
+		if diff=="1":
+			motList=diC["facile"]
+			wordChoosen=random.choice(motList)
+			print(wordChoosen)
+
+			d=input('waiter1 word')
+			exiter=1
+
+		elif diff=="2":
+			motList=diC["intermediaire"]
+			wordChoosen=random.choice(motList)
+	    
+			print(wordChoosen)
+
+			d=input('waiter1.2 word ')
+			exiter=1
+
+		elif diff=="3":
+			motList=diC["difficile"]
+			wordChoosen=random.choice(motList)
+	    
+			print(wordChoosen)
+
+			d=input('waiter1.3 word ')
+			exiter=1
+
+		else:
+			while True:
+				print("Erreur, entrée invalide, entrer un nombre de 1 à 3 de la selection :")
+
+				diff=input(" : ")
+				if diff in ['1','2','3']:
+					break
+				else:
+					...
+
+		if exiter ==1:
+			break
+	return wordChoosen
+#====
+elWord=gameword(1)
+
+userName=welcome()
+print("your username is : "+userName)	
+input("waiter #dev")
 
 #	start time, start game
 start_time = time.time()
-
-result=game("chaleté")
+input('waiter for time')
+result=[True,12]
 
 print("\nResult : ",result)
 
 end_time = time.time()
 
-time = end_time-start_time
-print("\n",time)
+ftime = end_time-start_time
+print("\n",ftime)
+
+print((userName,elWord,result[0],int(ftime)))
+print(type(userName),type(elWord),type(result[0]),type(int(ftime)))
+
 
